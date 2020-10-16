a1424b116bd2855f6a811f7659943d98507e34ca
Zubier <zubierabd@gmail.com>
2020-10-15 23:29:02 -0400

    made minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import zubier from '../assets/json/zubier.json'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -28,16 +28,12 @@ export class AppComponent {
 
 
   public firstLine = this.addresseArray[Math.floor(Math.random() * this.addresseArray.length) + 1] + ' ,'
-  public firstParagraph = `This letter is to express my interest in your posting on LinkedIn.com for the position of an experienced ${this.jobTitle} .  With a Master’s degree in Electrical and Computer Engineering, as well as over ${this.yearsOfExp} years hands-on experience working in the Canadian tech space, 
-  I am confident I will be an asset to your organization. `
-  public secondPara = `I am someone who continually seeks challenge and greatly enjoy learning new things and working on projects which require me to work outside my comfort zone. 
-  I enjoy learning new languages and frameworks in the web development sphere, as over the past two years in the field, I have worked with a number of technologies and am equally comfortable working in the front end as well as the back. 
-  At my current workplace (Big 5 Bank in Toronto – Capital Markets), I have worked to redesign and redevelop the existing customer administration modules of our Online Banking for Business platform, adhering to web accessibility guidelines 
-  and industry best practices. Aside from that, I have also worked on projects related to mitigating security vulnerabilities for the online banking platform as well as fraud mitigation through integrating IBM Trusteer Pinpoint API into the current product flow. `
-  public thirdPara = `Your listed requirements match my background and skill.`
-  public fourthPara = `Aside from professionally, my hobbies are also testament to my inquisitive and hardworking nature. I am an internationally published short story author. Details of my writing can be found at my website linked here.`
-  public fifthPara = `I have attached a copy of resume which details my projects and experience in software development. I can be reached anytime via cell phone at ${this.phoneNo} or email at ${this.email}`
-  public finalPara = 'Thank you for your time and consideration for my application. I look forward to hearing from you. '
+  public firstParagraph = ` `
+  public secondPara = zubier.secondP
+  public thirdPara = zubier.thirdP
+  public fourthPara = zubier.fourthP
+  public fifthPara = ``
+  public finalPara = zubier.finalP
 
   public name = ''
 
@@ -71,12 +67,21 @@ export class AppComponent {
       this.yearsOfExp = '3'
     }
 
-    this.firstParagraph = `This letter is to express my interest in your posting on LinkedIn.com for the position of an experienced ${this.jobTitle} ${this.companyName ? " at " + this.companyName : ''}.  With a Master’s degree in Electrical and Computer Engineering, as well as over ${this.numberWordMap[this.yearsOfExp]} years hands-on experience working in the Canadian tech space, 
-    I am confident I will be an asset to the organization. `
+    //Making replacements for paragraph one
+
+    this.firstParagraph = zubier.firstP
+    this.firstParagraph = this.firstParagraph.replace('_jobTitle_', this.jobTitle)
+    this.firstParagraph = this.firstParagraph.replace('_yearsExp_', this.numberWordMap[this.yearsOfExp])
+
+
     this.bulletPoint1 = this.bulletPoint1
     this.bulletPoint2 = this.bulletPoint2
     this.bulletPoint3 = this.bulletPoint3
-    this.fifthPara = `I have attached a copy of resume which details my projects and experience in software development. I can be reached anytime via cell phone at ${this.phoneNo} or email at ${this.email}`
+    ///Making replacements for fifth paragraph
+    this.fifthPara = zubier.fifthP
+    console.log(this.fifthPara)
+    this.fifthPara = this.fifthPara.replace('_email_', this.email)
+    this.fifthPara = this.fifthPara.replace('_phoneNo_', this.phoneNo)
     // this.yearsOfExp = this.numberWordMap[this.yearsOfExp]
     this.name = this.name;
     this.showSpinner = true;
